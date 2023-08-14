7d059061dec871d4e3830591a166904befa90171
KymRhys2k22 <kymrhys@gmail.com>
2023-08-14 23:43:29 +0800

    add dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-auto h-screen flex justify-center content-center '>
-      <iframe src="https://embed.lottiefiles.com/animation/88867"></iframe>
-      </div>
+    <>
+
+    </>
   )
 }
 
