50c1291da7fc79a2f25cf45ce000895ee8f7c317
KymRhys2@k22 <kymrhys@gmail.com>
2024-12-06 11:58:29 +0800

    try to restored

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+import typography from "@tailwindcss/typography";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -8,6 +10,6 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
   darkMode: "class",
 };
