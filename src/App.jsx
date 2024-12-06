9838c483e386f0f63246ec4a1c5c85a2f4bf122a
KymRhys2@k22 <kymrhys@gmail.com>
2024-12-06 11:39:33 +0800

    remove: loading state.

--- src/App.jsx
+++ src/App.jsx
@@ -11,53 +11,6 @@ import Aside from "./components/Aside";
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
-  //!const doc_ready = window.document.readyState
-
-  // This will run one time after the component mounts
-  /* useEffect(() => {
-    // callback function to call when event triggers
-    const onPageLoad = () => {
-      console.log('page loaded');
-      setIsLoading(false);
-      // do something else
-    };
-
-    // Check if the page has already loaded
-    if (document.readyState === 'complete') {
-      onPageLoad();
-    } else {
-      window.addEventListener('load', onPageLoad, false);
-      // Remove the event listener when component unmounts
-      return () => window.removeEventListener('load', onPageLoad);
-    }
-  }, []); */
-
-  /* useEffect(() => {
-    // Simulate loading time
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000); // 2 seconds loading time
-
-    return () => clearTimeout(timer);
-  }, []); */
-
-  /* useEffect(() => {
-    if (doc_ready === 'complete') {
-      setIsLoading(false);
-      if (doc_ready !== 'interactive') {
-        setIsLoading(false)
-      } else {
-        setIsLoading(true);
-        setIsLoading(true)
-      }
-    }
-  }, [doc_ready]); */
-
-  /*  if (isLoading) {
-    return <LoadingSpinner />;
-  }
- */
-
   return (
     <div
       className={`min-h-screen   bg-gray-950 overflow-x-hidden ${
